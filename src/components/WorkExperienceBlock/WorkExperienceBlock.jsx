8531a7907011526ee2657540dd72733cfa912249
GodoyMS <peruvalmunidial@gmail.com>
2025-02-17 09:52:29 -0500

    removed uuiid

--- src/components/WorkExperienceBlock/WorkExperienceBlock.jsx
+++ src/components/WorkExperienceBlock/WorkExperienceBlock.jsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import { FaLink } from "react-icons/fa";
-import { v4 as uuidv4 } from "uuid";
 
 const WorkExperienceBlock = () => {
   const info = [
